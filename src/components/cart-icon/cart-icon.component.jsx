139b08044e9c2ef58db9dd5a780379e664f39da6
mrunalselokar <mrunal.selokar.20@gmail.com>
2023-09-30 19:19:54 +0530

    added cart icon component and cart context

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import "./cart-icon.styles.scss";
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+
+const CartIcon = () => {
+	const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+	const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+	return (
+		<div className='cart-icon-container'>
+			<ShoppingIcon
+				className='shopping-icon'
+				onClick={toggleIsCartOpen}
+			/>
+			<span className='item-count'>10</span>
+		</div>
+	);
+};
+
+export default CartIcon;
