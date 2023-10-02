d69321c806143112c109a1998c644504a05369e1
mrunalselokar <mrunal.selokar.20@gmail.com>
2023-10-02 11:01:07 +0530

    updated cart total value

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -4,8 +4,7 @@ import CheckoutItem from "../../components/checkout-item/checkout-item.component
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-	const { cartItems, addItemToCart, removeItemFromCart } =
-		useContext(CartContext);
+	const { cartItems, cartTotal } = useContext(CartContext);
 	return (
 		<div className='checkout-container'>
 			<div className='checkout-header'>
@@ -34,7 +33,7 @@ const Checkout = () => {
 					/>
 				);
 			})}
-			<span className='total'>Total: 0</span>
+			<span className='total'>Total: ${cartTotal}</span>
 		</div>
 	);
 };
