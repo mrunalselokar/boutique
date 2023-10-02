43895a4abc15f93022f7ec9697c05137a851a896
mrunalselokar <mrunal.selokar.20@gmail.com>
2023-10-02 08:43:42 +0530

    increment item quantity on checkouts page

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 
-const addCartItem = (cartItems, productToAdd) => {
+export const addCartItem = (cartItems, productToAdd) => {
 	const cartItemMatches = (cartItem, productToAdd) => {
 		return cartItem.id === productToAdd.id;
 	};
@@ -23,7 +23,7 @@ export const CartContext = createContext({
 	isCartOpen: false,
 	setIsCartOpen: () => {},
 	cartItems: [],
-	addItemsToCart: () => {},
+	addItemToCart: () => {},
 	cartCount: 0,
 });
 
@@ -31,9 +31,7 @@ export const CartProvider = ({ children }) => {
 	const [isCartOpen, setIsCartOpen] = useState(false);
 	const [cartItems, setCartItems] = useState([]);
 	const [cartCount, setCartCount] = useState(0);
-	const addItemToCart = (productToAdd) => {
-		setCartItems(addCartItem(cartItems, productToAdd));
-	};
+
 	useEffect(() => {
 		const newCartCount = cartItems.reduce(
 			(total, cartItem) => total + cartItem.quantity,
@@ -42,12 +40,16 @@ export const CartProvider = ({ children }) => {
 		setCartCount(newCartCount);
 	}, [cartItems]);
 
+	const addItemToCart = (productToAdd) =>
+		setCartItems(addCartItem(cartItems, productToAdd));
+
 	const value = {
 		isCartOpen,
 		setIsCartOpen,
-		addItemToCart,
 		cartItems,
+		addItemToCart,
 		cartCount,
 	};
+
 	return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,9 +1,28 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import "./checkout.styles.scss";
 
 const Checkout = () => {
+	const { cartItems, addItemToCart } = useContext(CartContext);
 	return (
 		<div>
 			<h1>Checkout</h1>
+			<div>
+				{cartItems.map((cartItem) => {
+					const { id, name, quantity } = cartItem;
+					return (
+						<div
+							className=''
+							key={id}>
+							<h1>{name}</h1>
+							<span> - </span>
+							<span>{quantity}</span>
+
+							<span onClick={() => addItemToCart(cartItem)}> + </span>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
