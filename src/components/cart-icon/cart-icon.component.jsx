c5a83769e532372401a801b2a2a8f969989c4869
mrunalselokar <mrunal.selokar.20@gmail.com>
2023-10-02 08:10:48 +0530

    Updated cart count based on items in cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,7 +4,7 @@ import "./cart-icon.styles.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 const CartIcon = () => {
-	const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+	const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 	const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 	return (
 		<div className='cart-icon-container'>
@@ -12,7 +12,7 @@ const CartIcon = () => {
 				className='shopping-icon'
 				onClick={toggleIsCartOpen}
 			/>
-			<span className='item-count'>10</span>
+			<span className='item-count'>{cartCount}</span>
 		</div>
 	);
 };
