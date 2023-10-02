0ee43e96e8f6a67f7b9e7c1f3048a4e787888a57
mrunalselokar <mrunal.selokar.20@gmail.com>
2023-10-02 19:48:30 +0530

    adding routing to homepage categories

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -11,7 +11,6 @@ export const CategoriesProvider = ({ children }) => {
 	useEffect(() => {
 		const getCategoriesMap = async () => {
 			const categoryMap = await getCategoriesAndDocuments("categories");
-			console.log(categoryMap);
 			satCategoriesMap(categoryMap);
 		};
 		getCategoriesMap();
