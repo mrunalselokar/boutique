32a1347e08868a044ea8bbfe860d1210df10ab4c
mrunalselokar <mrunal.selokar.20@gmail.com>
2023-10-07 20:25:48 +0530

    refactoring categories selector

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,14 +5,14 @@ import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
-import { setCategoriesMap } from "../../store/categories/category.action";
+import { setCategories } from "../../store/categories/category.action";
 
 const Shop = () => {
 	const dispatch = useDispatch();
 	useEffect(() => {
 		const getCategoriesMap = async () => {
-			const categoryMap = await getCategoriesAndDocuments("categories");
-			dispatch(setCategoriesMap(categoryMap));
+			const categoriesArray = await getCategoriesAndDocuments("categories");
+			dispatch(setCategories(categoriesArray));
 		};
 		getCategoriesMap();
 	}, [dispatch]);
